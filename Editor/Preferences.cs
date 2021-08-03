beedf8c18866d8a339c5efeeb6672721a362cc2a
Walter Hulsebos <walterhulsebos@outlook.com>
2021-08-03 09:38:01 +0200

    NEW: Script Templates Settings Provider (class)
    This is what Unity needs to actually display the tool's prefs in the Preferences Window.

--- Editor/Preferences.cs
+++ Editor/Preferences.cs
@@ -87,6 +87,45 @@ namespace CGTK.Tools.CustomScriptTemplates
         public static Boolean UseAbsolutePath => !UseRelativePath;
         #endif
     }
+    
+    public sealed class ScriptTemplatesSettingsProvider : SettingsProvider
+    {
+        [PublicAPI]
+        public ScriptTemplatesSettingsProvider(in String path, in SettingsScope scopes, in IEnumerable<String> keywords = null) : base(path, scopes, keywords)
+        { }
+
+        public override void OnGUI(String searchContext)
+        {
+            #if ODIN_INSPECTOR
+            //Preferences.UseRelativePath = EditorGUILayout.Toggle(label: new GUIContent(text: "Project Relative Folder"), value: Preferences.UseRelativePath);
+            
+            EditorGUILayout.BeginHorizontal();
+            Preferences.TemplatesFolder = SirenixEditorFields.FolderPathField(label: new GUIContent(text: "Script Templates Folder"), path: Preferences.TemplatesFolder, parentPath: "Assets", absolutePath: Preferences.UseAbsolutePath, useBackslashes: false);
+            if (GUILayout.Button(text: "Reset", options: GUILayout.Width(80)))
+            {
+                Preferences.TemplatesFolder = Preferences.DefaultTemplatesFolder;
+            }
+            EditorGUILayout.EndHorizontal();
+            #else
+            Preferences.TemplatesFolder = EditorGUILayout.TextField(label: "Script Templates Folder", text: Preferences.TemplatesFolder);
+            #endif
+            
+            if (GUILayout.Button(text: "Regenerate Templates"))
+            {
+                ScriptTemplateFactory.RemoveAll();
+                ScriptTemplateFactory.CreateAll();    
+            }
+            
+            if (GUILayout.Button(text: "Reset Templates"))
+            {
+                ScriptTemplateFactory.RemoveAll();
+            }
+        }
+
+        [SettingsProvider]
+        public static SettingsProvider Create() 
+            => new ScriptTemplatesSettingsProvider(path: Constants.PREFERENCE_PATH, scopes: SettingsScope.User);
+    }
 }
 
 #endif
